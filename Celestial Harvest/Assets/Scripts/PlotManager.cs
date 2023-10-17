0c3b5670fd81034713eb323bd6791ac9e4a2dfae
katherinehoog19 <katherinehoog19@gmail.com>
2023-10-17 11:06:22 -0500

    crop grow cycle
    the crops grow on a timer (will need to change for a day night cycle in the future)

--- Celestial Harvest/Assets/Scripts/PlotManager.cs
+++ Celestial Harvest/Assets/Scripts/PlotManager.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class PlotManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    bool isPlanted = false;
+    SpriteRenderer plantSprite;
+    int plantStage = 0;
+    float timer;
+
+    public PlantObject selectedPlant;
+
+    void Start(){
+        plantSprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        if (isPlanted){
+            timer -= Time.deltaTime;
+
+            if (timer < 0 && plantStage < selectedPlant.plantStages.Length - 1){
+                timer = selectedPlant.timeBtwStages;
+                plantStage++;
+                UpdatePlant();
+            }
+        }
+    }
+
+    void OnMouseDown(){
+        if (isPlanted){
+
+            if (plantStage == selectedPlant.plantStages.Length - 1){
+                Harvest();
+            }
+        }
+
+        else{
+            Plant();
+        }
+    }
+
+    void Harvest(){
+        isPlanted = false;
+        if (plantSprite){
+            plantSprite.gameObject.SetActive(false);
+            plantSprite.gameObject.SetActive(true);
+        }
+    }
+
+    void Plant(){
+        isPlanted = true;
+        plantStage = 0;
+        UpdatePlant();
+        timer = selectedPlant.timeBtwStages;
+        plantSprite.gameObject.SetActive(true);
+    }
+
+    void UpdatePlant (){
+        plantSprite.sprite = selectedPlant.plantStages[plantStage];
     }
 }
