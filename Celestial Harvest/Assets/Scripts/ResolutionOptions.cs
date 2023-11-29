a8b37503382801b510973f27f67e1a67963ce06e
katherinehoog19 <katherinehoog19@gmail.com>
2023-11-29 13:58:42 -0600

    resolution and fullscreen options

--- Celestial Harvest/Assets/Scripts/ResolutionOptions.cs
+++ Celestial Harvest/Assets/Scripts/ResolutionOptions.cs
@@ -3,18 +3,92 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Resolution : MonoBehaviour
+public class ResolutionOptions : MonoBehaviour
 {
     [SerializeField] Dropdown resolutionDropdown;
-
+    [SerializeField] Toggle toggle;
     Resolution[] resolutions;
 
     void Start(){
         resolutions = Screen.resolutions;
 
-        for (int i = 0; i < resolutions.Length; i++){
+        bool setDefault = false;
+        if(PlayerPrefs.GetInt("set default resolution") == 0){
+            setDefault = true;
+            PlayerPrefs.GetInt("set default resolution",1);
+        }
+
+        for(int i = 0; i<resolutions.Length; i++){
             string resolutionString = resolutions[i].width.ToString() + " x " + resolutions[i].height.ToString();
             resolutionDropdown.options.Add(new Dropdown.OptionData(resolutionString));
+            if(setDefault && resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height){
+                resolutionDropdown.value = i;
+            }
+        }
+        toggle.isOn = PlayerPrefs.GetInt("fullscreen") == 0;
+        resolutionDropdown.value = PlayerPrefs.GetInt("resolution selection");
+
+    }
+
+    public void ChangeResolution(){
+         Screen.SetResolution(resolutions[resolutionDropdown.value].width,resolutions[resolutionDropdown.value].height,toggle.isOn);
+        PlayerPrefs.SetInt("resolution selection",resolutionDropdown.value);
+
+
+    }
+    public void ChangeFullscreen(){
+        Screen.SetResolution(resolutions[resolutionDropdown.value].width,resolutions[resolutionDropdown.value].height,toggle.isOn);
+        if(toggle.isOn){
+            PlayerPrefs.SetInt("f ullscreen",0);
+        }else{
+            Debug.Log("fullscreen off");
+            PlayerPrefs.SetInt("fullscreen",1);
         }
     }
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class ResolutionOptions : MonoBehaviour
+// {
+//     [SerializeField] Dropdown resolutionDropdown;
+//     [SerializeField] Toggle toggle;
+
+//     Resolution[] resolutions;
+
+//     void Start(){
+//         resolutions = Screen.resolutions;
+//         toggle.isOn = PlayerPrefs.GetInt("fullscreen") == 0;
+
+//         bool setDefault = false;
+
+//         if (PlayerPrefs.GetInt("set default resolution") == 0){
+//             setDefault = true;
+//             PlayerPrefs.GetInt("set default resolution", 1);
+//         }
+
+//         for (int i = 0; i < resolutions.Length; i++){
+//             string resolutionString = resolutions[i].width.ToString() + " x " + resolutions[i].height.ToString();
+//             resolutionDropdown.options.Add(new Dropdown.OptionData(resolutionString));
+//             if (setDefault && resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height){
+//                 resolutionDropdown.value = i;
+//             }
+//         }
+        
+//         resolutionDropdown.value = PlayerPrefs.GetInt("resolution selection");
+//     }
+
+//     public void changeResolution(){
+//         Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, toggle.isOn);
+//         PlayerPrefs.SetInt("resolution selection", resolutionDropdown.value);
+//         if (toggle.isOn){
+//             PlayerPrefs.SetInt("fullscreen", 0);
+//         }
+//         else {
+//             PlayerPrefs.SetInt("fullscreen", 1);
+//         }
+//     }
+// }
